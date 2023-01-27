4cf2e88992fe36145d630cf90bfaafdd363d9238
sonrisahy <sonrisa@cap.ocn.ne.jp>
2023-01-27 15:52:38 +0900

    commit 2

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,16 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Person
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        ('Personal Info', {'fields': ('username', 'email', 'password',)}),
+        (None, {'fields': ('is_active', 'is_admin',)}),
+    )
+    list_display = ('username', 'email', 'is_active',)
+    serach_fields = ('username')
+    list_filter = ()
+    ordering = ()
+    filter_horizontal = ()
+
+admin.site.register([Person])
